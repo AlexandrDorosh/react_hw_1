ba5908848551d27d9abab4aa8198632982e84177
AlexandrDorosh <dovgaloleksandr388@gmail.com>
2021-06-23 11:05:58 +0300

    fixed and add hw_1

---
+++ src/posts/posts.js
@@ -0,0 +1,24 @@
+import './posts.css';
+import {useEffect, useState} from "react";
+import Post from "./post/post";
+
+export default function Posts() {
+
+    let [postsList, setPostsList] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => value.json())
+            .then(value => {
+                setPostsList(value);
+            })
+    }, []);
+    return(
+        <div className='posts'>
+            <h1>Posts</h1>
+            {
+                postsList.map(value => <Post key={value.id} items={value}/>)
+            }
+        </div>
+    )
+}
